b849e3dbc22f59aa88575604cdfece319725f82d
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-11-03 22:20:08 +0530

    Initialize the project

---
+++ src/main/java/com/blogapi/blogplatform/model/User.java
@@ -0,0 +1,16 @@
+package com.blogapi.blogplatform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "blog_users")
+public class User {
+
+}
